5c6108f1a3bdbd48f394fd89c21d8cbefca306b6
Laura <laurabande10@gmail.com>
2021-02-01 19:08:39 +0100

    Declaración de atributos y métodos en la clase Empleado

--- src/main/java/jerarquia/Empleado.java
+++ src/main/java/jerarquia/Empleado.java
@@ -1,5 +1,32 @@
 package jerarquia;
 
 public class Empleado {
+	
+	private double salario;
 
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+
+	public Empleado(double salario) {
+		super();
+		this.salario = salario;
+	}
+
+	public Empleado() {
+		super();
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + "Empleado [salario=" + salario + "]";
+	}
+	
+	public void aumentarSalario(double aumento) {
+		salario += aumento;
+	}
 }
